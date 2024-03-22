6e41fe5c2c7265ee2bac97d2e7b246270be0870d
Ousama Jelassi <jls.ousama@gmail.com>
2024-03-22 10:48:17 +0100

    Changing Directory
    Changing Photo and audio directory to GIT HUB

---
+++ MODALE APP/app.js
@@ -0,0 +1,12 @@
+// Mostra la modale
+function mostraModale() {
+    var modale = document.getElementById('modale');
+    modale.style.display = 'block';
+  }
+  
+  // Nasconde la modale
+  function chiudiModale() {
+    var modale = document.getElementById('modale');
+    modale.style.display = 'none';
+  }
+  
